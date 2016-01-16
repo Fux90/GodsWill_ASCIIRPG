a869bd1f9e9ba6bd2dc2dd28ad175ac743167ba1
ff <f.fucci@outlook.com>
2016-01-16 23:46:56 +0100

    Test Commit
    Some issues...

---
+++ Model/AtomCollection.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
+
+namespace GodsWill_ASCIIRPG.Model
+{
+    public class AtomCollection : Atom, ICollection<Atom>
+    {
+        List<Atom> atoms;
+
+        public int Count
+        {
+            get
+            {
+                return atoms.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public AtomCollection()
+            : base("Collection", "C", Color.White, true, false)
+        {
+            atoms = new List<Atom>();
+        }
+
+        public Atom this[int index]
+        {
+            get
+            {
+                return atoms[index];
+            }
+        }
+
+        public void Add(Atom atom)
+        {
+            atoms.Add(atom);
+        }
+
+        public void Clear()
+        {
+            atoms.Clear();
+        }
+
+        public bool Contains(Atom atom)
+        {
+            return atoms.Contains(atom);
+        }
+
+        public void CopyTo(Atom[] array, int arrayIndex)
+        {
+            atoms.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(Atom atom)
+        {
+            return atoms.Remove(atom);
+        }
+
+        public override bool Interaction(Atom interactor)
+        {
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public AtomCollectionEnum GetEnumerator()
+        {
+            return new AtomCollectionEnum(atoms.ToArray());
+        }
+
+        IEnumerator<Atom> IEnumerable<Atom>.GetEnumerator()
+        {
+            return new AtomCollectionEnum(atoms.ToArray());
+        }
+
+        public void ForEach(Action<Atom> action)
+        {
+            atoms.ForEach(action);
+        }
+    }
+
+    public class AtomCollectionEnum : IEnumerator<Atom>
+    {
+        Atom[] atoms;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public AtomCollectionEnum(Atom[] list)
+        {
+            atoms = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < atoms.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public void Dispose()
+        {
+            //
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Atom Current
+        {
+            get
+            {
+                try
+                {
+                    return atoms[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
