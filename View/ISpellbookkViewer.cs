9afb1e27ab62cc7f4ba920eb2a506c778cabfb7f
ff <f.fucci@outlook.com>
2016-01-23 14:38:18 +0100

    Begun spellbook control mechanics

---
+++ View/ISpellbookkViewer.cs
@@ -0,0 +1,14 @@
+﻿using GodsWill_ASCIIRPG.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GodsWill_ASCIIRPG.View
+{
+    public interface ISpeelbookViewer : IViewer
+    {
+        void NotifyAdd(Spell[] spells);
+    }
+}
